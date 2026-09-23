d0f1d2305a82c8c46c75600f9361ffe4182c85c8
Robert Haughton <152531780+robhaughton1@users.noreply.github.com>
2026-09-22 23:49:02 -0400

    Add Word model for spell checker

---
+++ Word.java
@@ -0,0 +1,11 @@
+public class Word {
+    public String originalWord;
+    public String candidateWord;
+    public int editDistance;
+
+    public Word(String originalWord, String candidateWord, int editDistance) {
+        this.originalWord = originalWord;
+        this.candidateWord = candidateWord;
+        this.editDistance = editDistance;
+    }
+}
